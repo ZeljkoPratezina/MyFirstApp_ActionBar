3f6e43195858377b4d0f52efbff149cbaa422305
ZeljkoPratezina <pratezina.zeljko@gmail.com>
2016-07-01 20:21:51 +0200

    Initial commit

---
+++ app/src/main/java/com/zeljkopratezina/myfirstapp_actionbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.zeljkopratezina.myfirstapp_actionbar;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setLogo(R.mipmap.ic_launcher);
+        ab.setDisplayUseLogoEnabled(true);
+        ab.setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.info_id:
+                Toast.makeText(getApplicationContext(),
+                        "Info icon is selected", Toast.LENGTH_SHORT).show();
+            case R.id.setting_id:
+                Toast.makeText(getApplicationContext(),
+                        "Setting icon is selected", Toast.LENGTH_SHORT).show();
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+}
